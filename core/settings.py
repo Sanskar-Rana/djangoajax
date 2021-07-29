ece2d73e57ce3fecce9923fd1115a1be4ed1451a
Sanskar-Rana <rsanskar31@gmail.com>
2021-07-29 22:53:08 +0545

    Fourth Commit

--- core/settings.py
+++ core/settings.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 import os
-import django_heroku
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["djangoajax033.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ['djangoajax033.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -105,10 +104,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
